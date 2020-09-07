8edd6da1cd3aa276979ad90d138eda17c40b0f14
Luis Sanchez <galaxyplayeremail@gmail.com>
2020-09-07 08:59:59 -0400

    Update jail_time.js
    misc

--- speedwagonfoundation/jail_time.js
+++ speedwagonfoundation/jail_time.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const SECRETSHIT = 'NzUyMzI2NDcyNDQxNzkwNDk0.X1WAoA.bfNn3GagSzfWaX9tcECGX29kJ3Y';
+const BOTTOKEN;
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -11,16 +11,4 @@ client.login(SECRETSHIT);
 
 client.on('message', message => {
     console.log(message.content);
-})
-
-client.on('message', message => {
-    if(message.author.username === 'Jaben'){
-        message.channel.send('Jaben is talking');
-    }
-})
-
-client.on('message', message => {
-    if(message.author.username === 'Atomsk'){
-        message.channel.send('Weeb shit incoming');
-    }
 })
